0a574bd44d7d2977ee4270b37a61e75c4f93f571
oksusu <113329728+oksusu8@users.noreply.github.com>
2023-12-07 23:37:04 +0900

    3rd

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ let canvas;
 
 function setup() {
   const canvasContainer = select('.camera-container');
+  //canvas = createCanvas(600, 400);
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent(canvasContainer);
 
@@ -16,4 +17,9 @@ function draw() {
   image(video, 0, 0, width, height);
 }
 
+function windowResized() {
+  // 윈도우 크기가 변경되면 canvas와 video 크기를 다시 조정
+  resizeCanvas(windowWidth, windowHeight);
+  video.size(width, height);
+}
 
