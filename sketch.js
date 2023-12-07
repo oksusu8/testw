441c8e9976dc2a8920f216b8d42cb26dd35ecdcd
oksusu <113329728+oksusu8@users.noreply.github.com>
2023-12-08 00:10:37 +0900

    4th

--- sketch.js
+++ sketch.js
@@ -1,25 +1,24 @@
 let video;
-let canvas;
+let capturedImage;
 
 function setup() {
-  const canvasContainer = select('.camera-container');
-  //canvas = createCanvas(600, 400);
-  canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent(canvasContainer);
-
+  createCanvas(windowWidth, windowHeight);
   video = createCapture(VIDEO);
+  video.size(width, height * 0.66);
   video.hide();
-  video.size(windowWidth, windowHeight);
+  
+  captureBtn = select('#captureBtn');
+  
+  captureBtn.mousePressed(talk);
+  
 }
 
 function draw() {
   background(0);
-  image(video, 0, 0, width, height);
+  image(video, 0, height * 0.08);
+  
 }
 
-function windowResized() {
-  // 윈도우 크기가 변경되면 canvas와 video 크기를 다시 조정
-  resizeCanvas(windowWidth, windowHeight);
-  video.size(width, height);
+function talk() {
+  console.log('촬영');
 }
-
